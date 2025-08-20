d8583ca151724c3a6529988e3b9f54eb7a176177
Matthew-R-P <mpollock8587@floridapoly.edu>
2025-08-20 18:42:07 +0000

    Created first programs using C++ in github

---
+++ 01_c++_intro/first_program.cpp
@@ -0,0 +1,21 @@
+/* 
+08/20/2025
+The First C++ program
+
+
+*/
+
+ #include <iostream>
+using namespace std;
+  // Create a main function
+
+int main(void) {
+    std::cout << "Hello COP3337!" << std::endl;
+
+    // std - standard namespace
+    // :: - scpore resolution operator
+    // cout - console output (object of an iostream class)
+    // << - stream insertion operator
+    // endl - end of line (pretty much like \n)
+    return 0;
+}
---
+++ namespaces.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Create the namespace
+namespace first {
+    int a = 5;
+
+    void print()  {
+        std::cout << "First" << std::endl;
+    }
+}
